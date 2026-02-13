f7e93102af6ae0617df69a82a52454374afe70ca
Aman Sha <amansha2001@gmail.com>
2026-02-13 23:04:05 +0530

    Update atm java code
    Added 4 choices in atm menu and successfully ran the code.

--- simple-atm-java/atm.java
+++ simple-atm-java/atm.java
@@ -1,7 +1,45 @@
-
+import java.util.Scanner;
 public class atm {
     public static void main(String[] args) {
-        System.out.println("Java is working");
-        System.out.println("Java is working right ");
+        Scanner scanner = new Scanner(System.in);
+        double balance = 1000.0;
+        int choice;
+        while(true) {
+            System.out.println("\n ======= ATM MENU ===");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+             if(choice == 1){
+                System.out.println("Your balance is: " + balance);
+
+             }
+             else if(choice == 2){
+                System.out.println("Enter the amount for deposit ");
+                double deposit = scanner.nextDouble();
+                balance = balance + deposit;
+                System.out.println("Amount deposited successfully");
+             }
+             else if(choice == 3){
+                System.out.println("Enter the amount to withdraw");
+                double withdraw = scanner.nextDouble();
+                if (withdraw <= balance){
+                    balance = balance - withdraw;
+                    System.out.println("Please collect your cash");
+                } else{
+                    System.out.println("Insufficient balance");
+                }
+             }
+             else if(choice == 4){
+                System.out.println("Thanks for using the atm");
+                break;
+             } else{
+                System.out.println("Invalid choice");
+             }
+
+        }
+        scanner.close();
     }
 }
